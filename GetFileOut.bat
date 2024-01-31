07a2c0144caf994629519faa42ef61b72d7e379a
unknown <hoyuysh@gmail.com>
2024-02-01 00:48:02 +0800

    add GetFileOut

---
+++ GetFileOut.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+:: Save the original path
+set OriDir=%CD%
+:: Get file location
+echo Input File Location
+set /p TarDir=
+::Ask if delete the empty directory
+echo Would you like to delete the directory? (Y/N)
+set /p DelDir=
+::Check input is valid
+if NOT "%DelDir%"=="Y" (
+    if NOT "%DelDir%"=="N" (
+        echo Please input Y or N
+        exit /B 0
+    )
+)
+:: Go to target directory
+cd %TarDir%
+::Move file
+call:GetFileOut
+:: Go back to original directory
+cd %OriDir%
+:: Finish
+echo Finish
+
+:GetFileOut
+for /D %%x in (*) do (
+    cd %%x
+    call:GetFileOut
+    for %%y in (*) do (
+        move "%%y" "%TarDir%"
+    )
+    cd ..
+    if "%DelDir%"=="Y" rmdir %%x
+)
+exit /B 0
