3e2678f278e559103953500cd6203f87b95f2c2d
unknown <hoyuysh@gmail.com>
2024-07-10 23:52:02 +0800

    New FindUserID

--- FindUserID.bat
+++ FindUserID.bat
@@ -1,17 +1,22 @@
 @echo off
 
+echo Where is the list?
 set /p Dir=
 
+echo Look for what field?
+set /p field=
+
+echo Search Start
 setlocal enabledelayedexpansion
 for /f %%U in (%Dir%) do (
     net user /domain %%U >nul 2>&1
     if !errorlevel! equ 0 (
-        for /f "tokens=1,2,* delims= " %%A in ('net user /domain %%U ^| find "Full Name"') do (
+        for /f "tokens=1,2,* delims= " %%A in ('net user /domain %%U ^| find %field%') do (
             echo %%U %%C >> Result.txt
         )
     )
 )
 endlocal
 
-echo finish
+echo Finish
 pause
