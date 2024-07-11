632679a07667f14f11b05b12795bda1a10bf4db6
unknown <hoyuysh@gmail.com>
2024-07-11 21:48:31 +0800

    New FindUserID

--- FindUserID.bat
+++ FindUserID.bat
@@ -11,7 +11,7 @@ setlocal enabledelayedexpansion
 for /f %%U in (%Dir%) do (
     net user /domain %%U >nul 2>&1
     if !errorlevel! equ 0 (
-        for /f "tokens=1,2,* delims= " %%A in ('net user /domain %%U ^| find %field%') do (
+        for /f "tokens=1,2,* delims= " %%A in ('net user /domain %%U ^| find "%field%"') do (
             echo %%U %%C >> Result.txt
         )
     )
